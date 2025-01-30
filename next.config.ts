a30ba6df1589d75e32fc4b406d1eb73e0dfae3b3
Jan Zdenek <ydenekjan@gmail.com>
2025-01-30 22:01:11 +0100

    refactor(header): cleaned up header

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "raw.githubusercontent.com",
+        port: "",
+        pathname: "/PokeAPI/sprites/master/sprites/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
--- src/components/header/headerImage.tsx
+++ src/components/header/headerImage.tsx
@@ -3,18 +3,18 @@ import headerBg from "../../../public/header-bg.jpg";
 
 const HeaderImage = () => {
   return (
-    <div className={"h-full w-full overflow-hidden absolute -z-30"}>
+    <div className="h-full w-full overflow-hidden absolute">
+      {/* Background Image */}
       <Image
         src={headerBg}
-        className={"object-cover h-full object-left opacity-30 -z-10"}
-        alt={"Pokémon background"}
+        className="object-cover h-full object-left opacity-30"
+        alt="Pokémon background"
         fill
+        priority
       />
-      <div
-        className={
-          "absolute top-0 left-0 h-full w-full z-0 bg-gradient-to-t from-background to-transparent"
-        }
-      ></div>
+
+      {/* Gradient Overlay */}
+      <div className="absolute inset-0 bg-gradient-to-t from-background to-transparent" />
     </div>
   );
 };
